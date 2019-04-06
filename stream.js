8a58c253a250ef263837de5a53f1ec837e9a2846
Robert Wolff <ro.wolff@gmx.net>
2019-04-06 14:18:52 +0200

    Only parse active games

--- stream.js
+++ stream.js
@@ -5,7 +5,7 @@ const getStream = async function() {
       "https://cors-anywhere.herokuapp.com/https://api.sporttotal.tv/v2/live?sporttypeuuid=6294114c-5400-4978-bc6a-b3fe75d03fdf&channeluuid=00365d04-ad90-4ef7-b9b3-ef1e72890908"
     );
     let data = await response.json();
-    stream = data[0].streamUrl;
+    stream = data.filter(item => item.status == "active")[0].streamUrl;
   }
   return stream;
 };
