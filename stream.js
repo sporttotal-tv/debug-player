e81ed1396978d58f1ad49f225208cdd5dbbda623
Robert Wolff <ro.wolff@gmx.net>
2019-04-06 14:14:11 +0200

    Bundle dependencies

--- stream.js
+++ stream.js
@@ -1,5 +1,5 @@
 const getStream = async function() {
-  let stream = undefined;
+  let stream = "https://content.jwplatform.com/manifests/yp34SRmf.m3u8";
   if (!stream) {
     let response = await fetch(
       "https://cors-anywhere.herokuapp.com/https://api.sporttotal.tv/v2/live?sporttypeuuid=6294114c-5400-4978-bc6a-b3fe75d03fdf&channeluuid=00365d04-ad90-4ef7-b9b3-ef1e72890908"
