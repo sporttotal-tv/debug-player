f29b4b297b4a4fe55f9f4e546f9d55192d15866c
Robert Wolff <ro.wolff@gmx.net>
2019-04-06 12:15:23 +0200

    Update stream url

--- stream.js
+++ stream.js
@@ -1,3 +1,3 @@
 const getStream = function() {
-  return "https://d1x5yrphsaflin.cloudfront.net/RECORD/0_hd_hls.m3u8?hlsid=HTTP_ID_1";
+  return "https://dfrlpoqduzbfx.cloudfront.net/RECORD/0_hd_hls.m3u8?hlsid=HTTP_ID_1";
 };
