c72f564dfbed7f279727d54c05184ff1b5a0889e
youzi <youridaamen@gmail.com>
2019-04-24 13:02:35 +0200

    clear local storage on init

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-import { setupVideojs } from "./modules/videojsplayer.js";
-import { setupJwplayer } from "./modules/jwplayer.js";
-import { getStreamUrlFromAPI } from "./modules/stream.js";
+import { setupVideojs } from './modules/videojsplayer.js'
+import { setupJwplayer } from './modules/jwplayer.js'
+import { getStreamUrlFromAPI } from './modules/stream.js'
 
 const setupPlayers = async function(
-  streamSrc = "https://content.jwplatform.com/manifests/yp34SRmf.m3u8"
+  streamSrc = 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8'
 ) {
   if (!streamSrc) {
-    streamSrc = await getStreamUrlFromAPI();
+    streamSrc = await getStreamUrlFromAPI()
   }
   //setupJwplayer(streamSrc);
-  setupVideojs(streamSrc);
-};
+  setupVideojs(streamSrc)
+}
 
 const getCustomStream = function() {
-  setupPlayers(document.getElementById("customstream").value);
-};
+  setupPlayers(document.getElementById('customstream').value)
+}
 
 const getActive = async function() {
-  document.getElementById("customstream").value = null;
-  setupPlayers(null);
-};
+  document.getElementById('customstream').value = null
+  setupPlayers(null)
+}
 
 const registerInputHandlers = function() {
   document
-    .getElementById("submitCustomStream")
-    .addEventListener("click", getCustomStream);
+    .getElementById('submitCustomStream')
+    .addEventListener('click', getCustomStream)
 
-  document.getElementById("loadActive").addEventListener("click", getActive);
-};
+  document.getElementById('loadActive').addEventListener('click', getActive)
+}
 
-registerInputHandlers();
+registerInputHandlers()
 //setupPlayers(null);
 setupPlayers(
-  "https://dptf639cz3dpg.cloudfront.net/RECORD/0_hd_hls.m3u8?hlsid=HTTP_ID_1"
-);
+  'https://dptf639cz3dpg.cloudfront.net/RECORD/0_hd_hls.m3u8?hlsid=HTTP_ID_1'
+)
--- modules/stream.js
+++ modules/stream.js
@@ -1,7 +1,7 @@
 export const getStreamUrlFromAPI = async function() {
   const response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://api.sporttotal.tv/v2/live?sporttypeuuid=6294114c-5400-4978-bc6a-b3fe75d03fdf&channeluuid=00365d04-ad90-4ef7-b9b3-ef1e72890908"
-  );
-  const data = await response.json();
-  return data.filter(item => item.status == "active")[0].streamUrl;
-};
+    'https://cors-anywhere.herokuapp.com/https://api.sporttotal.tv/v2/live?sporttypeuuid=6294114c-5400-4978-bc6a-b3fe75d03fdf&channeluuid=00365d04-ad90-4ef7-b9b3-ef1e72890908'
+  )
+  const data = await response.json()
+  return data.filter(item => item.status == 'active')[0].streamUrl
+}
